6eba185e7e87cc440df9103ce66122e192fb6fd4
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-04-23 16:35:53 +0800

    Simplify App.js to only include ChoiceLight functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
-import BingoGame from "./components/BingoGame/BingoGame";
 import ChoiceLightList from "./components/ChoiceLight/ChoiceLightList";
-import Login from "./components/Login/Login";
-import ChoiceGame from "./components/ChoiceGame";
-import BikeGame from "./components/BikeGame";
-import CardManagement from "./components/BikeGame/CardManagement";
-
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 // Version display component
 const VersionDisplay = () => {
@@ -37,15 +31,8 @@ function App() {
       <div className="App">
         <VersionDisplay />
         <Routes>
-          <Route path="/bingo-game" element={<BingoGame />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/choice-game" element={<ChoiceGame />} />
           <Route path="/choice-light" element={<ChoiceLightList />} />
-          <Route path="/bike-game" element={<BikeGame />} />
-          <Route
-            path="/card-management/practice"
-            element={<CardManagement />}
-          />
+          <Route path="/" element={<Navigate to="/choice-light" />} />
         </Routes>
       </div>
     </Router>
